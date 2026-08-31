dbae8a9db72b230382911d95dd345b09c32c6b8b
Can Kayser <kayser_can@live.de>
2026-08-31 21:34:48 +0200

    update random search

--- src/ray_random_search.py
+++ src/ray_random_search.py
@@ -9,36 +9,43 @@ from ray.air.integrations.wandb import WandbLoggerCallback
 
 from config.ray.pbt import CONFIG
 from config.ray.utils import PatienceStopper
-from ray_training import train
+from ray_training import train, train_catch_oom
 from utils.settings import PROJECT_NAME
+from config.ray.all import ID
 
 MAX_HOURS = 70
 
-train = tune.with_resources(train, {"cpu": 1, "gpu": 1})
+
+train = tune.with_resources(
+	train_catch_oom,
+	{
+		"cpu": 4,
+		"gpu": (1/2 if torch.cuda.is_available() else 0),
+		"memory": 12 * 1024**3
+	}
+)
+
+
 def tune_with_callback():
 	ray.init()
-
 	tuner = tune.Tuner(
 		train,
 		tune_config=tune.TuneConfig(
         	search_alg=BasicVariantGenerator(),
-			metric='test_loss',
-			mode='min',
+			metric="evaluation",
+			mode="min",
 			num_samples=-1,
-			time_budget_s=MAX_HOURS * 60 * 60,  # total wall-clock time in hours
+			time_budget_s = MAX_HOURS * 60 * 60,  # total wall-clock time in hours
 		),
 		run_config=tune.RunConfig(
-			keep_checkpoints_num=1,
-			checkpoint_score_attr="test_loss",
+			name=CONFIG['id'],
 			verbose=1,
-			stop=PatienceStopper(
-					metric="test_loss",
-					patience=CONFIG['patience'],
-				),
-        	callbacks=[WandbLoggerCallback(
-					project=PROJECT_NAME,
-					group=CONFIG['id'],
-				)]
+			# stop=stopper,
+        	# callbacks=[WandbLoggerCallback(
+			# 		project=PROJECT_NAME,
+			# 		group=CONFIG['id'],
+			# 		log_config=True,
+			# 	)]
 		),
 		param_space=CONFIG,
 	)
@@ -52,34 +59,20 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter
 	)
-
-	parser.add_argument("-st", "--surrogate_type", type=str, default='default', help="The type of surrogate gradient to use")
-	parser.add_argument("-id", "--id", type=str, default='default', help="An ID for WandB")
+	parser.add_argument("-id", "--id", type=str, default=f"{ID}", help="An ID for WandB")
+	parser.add_argument("-a", "--architecture", type=str, default='', help="Architecture String")
 
 	try:
 		args = parser.parse_args()
 	except SystemExit:
 		args = parser.parse_args([])
-		raise Warning('Could not parse arguments, falling back to default values.')
+		# raise Warning('Could not parse arguments, falling back to default values.')
 
-	surrogate_type = args.surrogate_type
-	ID = args.id
+	CONFIG['id'] = args.id
+	CONFIG['architecture'] = args.architecture
+	CONFIG['already_initialised'] = []
 
-	if ID == 'default':
-		ID = surrogate_type
-
-
-	CONFIG['id'] = ID
-
-	if surrogate_type == 'default':
-		CONFIG['type'] = tune.grid_search(['gauss', 'laplace', 'cauchy'])
-	else:
-		CONFIG['type'] = surrogate_type
-
-
-	result_grid = tune_with_callback()
-	df = result_grid.get_dataframe()
-	df.to_csv(f'surrogate_search.csv')
-
+	with torch.autograd.detect_anomaly(check_nan=True):
+		tune_with_callback()
 
 # %%
